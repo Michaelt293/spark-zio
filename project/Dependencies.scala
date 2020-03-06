ccb2b3f4bbdefb204e131e0c08ea23ef9ce53ec8
Michael Thomas <Michaelt293@gmail.com>
2020-03-06 20:22:19 +1100

    zio version bump

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val zio = "dev.zio" %% "zio" % "1.0.0-RC11-1"
+  lazy val zio = "dev.zio" %% "zio" % "1.0.0-RC18-1"
   lazy val sparkSql = "org.apache.spark" %% "spark-sql" % "2.4.3"
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
 }
